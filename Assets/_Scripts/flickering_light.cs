f2bae5f57bf26c9fb1b1c368879a5b1399fec829
Nirmax96 <nirmaxmercenario@gmail.com>
2025-09-15 23:22:38 -0300

    cambio de iluminacion
    se crea el parpadeo de luz
    se cambia la iluminacion de lava shrine
    se crea el script para el parpadeo
    se crean prefabs nuevos

---
+++ Assets/_Scripts/flickering_light.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+[RequireComponent(typeof(Light))]
+public class flickering_light : MonoBehaviour
+{
+  private Light lightoFlicker;
+  [SerializeField, Range(0f, 12f)] private float minIntensity = 6.8f;
+  [SerializeField, Range(0f, 12f)] private float maxIntensity = 2.4f;
+  [SerializeField, Min(0f)] private float timeBetweenIntensity = 0.1f;
+
+    private float currentTimer;
+    private void Awake()
+    {
+        if (lightoFlicker == null)
+        {
+        lightoFlicker = GetComponent<Light>();
+        }
+
+        ValidateIntensityBounds();
+    }
+
+    private void Update()
+    {
+        currentTimer+=Time.deltaTime;
+        if (!(currentTimer >= timeBetweenIntensity)) return;
+        lightoFlicker.intensity = Random.Range(minIntensity, maxIntensity);
+        currentTimer=0;
+    }
+
+    private void ValidateIntensityBounds()
+    {
+        if (!(minIntensity > maxIntensity))
+        {
+            return;
+        }
+        Debug.LogWarning("Min Intensity is greater than max Intensity, Swapping Values!");
+        (minIntensity, maxIntensity) = (maxIntensity, minIntensity);
+    }
+}
