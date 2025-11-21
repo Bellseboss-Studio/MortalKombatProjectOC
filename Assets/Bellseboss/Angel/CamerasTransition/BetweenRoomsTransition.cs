f55a4361549655daed4d953cc9deca0c5ef1e589
Nirmax96 <nirmaxmercenario@gmail.com>
2025-11-21 11:54:19 -0300

    Revert "Eliminacion de carpeta 2"
    This reverts commit dde6ced04663d30c1c41ee27520bc1d245ab2856.

---
+++ Assets/Bellseboss/Angel/CamerasTransition/BetweenRoomsTransition.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using _Scripts.Interact;
+using DG.Tweening;
+using UnityEngine;
+
+namespace Bellseboss.Angel
+{
+    [Serializable]
+    public class BetweenRoomsTransition
+    {
+        public CameraCollider finishingRoom;
+        public Transform[] cameraPoints;
+        public float transitionTime;
+        public Ease easeType;
+    }
+}
